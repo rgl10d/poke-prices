3e24fd91dc5aaef8ebe0e31579b3d5bb46350ef6
Garrett Lee <rgl10d@gmail.com>
2020-10-22 19:24:38 -0400

    small bit of styling on search bar and card results

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ console.log("It's working! It's working!");
 
 
 
-$("#nav-search-btn").on("click", function (event) {
+$("#srch-button").on("click", function (event) {
     $("#display-card").html("");
-    userSearch = $("#this-is-a-search-bar").val();
+    userSearch = $("#srch-bar").val();
     pokemoncardURL = "https://api.pokemontcg.io/v1/cards?name=" + userSearch;
     event.preventDefault();
-    console.log(userSearch);
     $.ajax({
         url: pokemoncardURL,
         method: "GET"
