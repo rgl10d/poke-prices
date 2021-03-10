7d6f3f905e9454ec60d474a205d35104a8a0e3c3
Garrett Lee <rgl10d@gmail.com>
2021-03-10 18:06:19 -0500

    moved files to client src folder

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("client/build"));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/mern-starter",
@@ -21,17 +24,21 @@ mongoose.connect(
 const connection = mongoose.connection;
 
 connection.on("connected", () => {
-    console.log("Mongoose successfully connected!")
+  console.log("Mongoose successfully connected!");
 });
 
 connection.on("error", (err) => {
-    console.log("Mongoose connection error: ", err)
+  console.log("Mongoose connection error: ", err);
 });
 
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(PORT, () => {
