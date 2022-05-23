c7207f4629391dafdc5d278506d9682d2b5cc06a
Garrett Lee <rgl10d@gmail.com>
2022-05-23 00:58:35 -0400

    added conditional statements to prevent sim from starting before data was received

--- client/src/containers/PackSimulator/PackSimulator.jsx
+++ client/src/containers/PackSimulator/PackSimulator.jsx
@@ -6,7 +6,7 @@ import pokemon from "pokemontcgsdk";
 pokemon.configure({ apiKey: "bda1ab63-5db0-43e0-8b1f-a50ba6b7fc4b" });
 
 const PackSimulator = () => {
-  const [cardList, setCardList] = useState();
+  const [cardList, setCardList] = useState(null);
   const [pack, setPack] = useState();
 
   const openPack = () => {
@@ -36,14 +36,29 @@ const PackSimulator = () => {
     );
   }
 
-  if (!pack) {
+  if (cardList === null) {
     return (
       <>
         <Navbar />
         <div>
           <h1>Pokemon card pack simulator.</h1>
 
-          <button className="btn btn-primary" onClick={openPack}>
+          <button className="btn btn-primary"  disabled onClick={openPack}>
+            Open a pack!
+          </button>
+        </div>
+      </>
+    );
+  }
+
+  if (cardList) {
+    return (
+      <>
+        <Navbar />
+        <div>
+          <h1>Pokemon card pack simulator.</h1>
+
+          <button className="btn btn-success" onClick={openPack}>
             Open a pack!
           </button>
         </div>
