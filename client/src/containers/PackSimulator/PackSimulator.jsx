33b47e23f2d507358bd72dc1378033d7d543a124
Garrett Lee <rgl10d@gmail.com>
2022-05-23 16:03:07 -0400

    opening packs now has multiple cards instead of one

--- client/src/containers/PackSimulator/PackSimulator.jsx
+++ client/src/containers/PackSimulator/PackSimulator.jsx
@@ -9,25 +9,31 @@ const PackSimulator = () => {
   const [cardList, setCardList] = useState(null);
   const [pack, setPack] = useState();
 
-  const openPack = () => {
-    setPack(cardList[[Math.floor(Math.random() * cardList.length)]]);
-    // console.log(cardList);
-  };
-
   // FETCH SPECIFIC SET CARD LIST ON PAGE LOAD
   useEffect(() => {
-    pokemon.card.where({ q: "set.id:base1 rarity:Rare" }).then((results) => {
+    pokemon.card.where({ q: "set.id:base1 rarity:Common" }).then((results) => {
       setCardList(results.data);
     });
-  }, []);
+  }, [cardList]);
+
+  // PACK LOGIC FUNCTION
+  const openPack = () => {
+    const commonArray = [];
+    for (let i = 0; i < 7; i++) {
+      commonArray.push(cardList[[Math.floor(Math.random() * cardList.length)]]);
+    }
+    setPack(commonArray);
+    // console.log(cardList);
+  };
 
   if (pack) {
     return (
       <>
         <Navbar />
         <div className="container">
-          <h1>{pack.name}</h1>
-          <img src={pack.images.small} alt={pack.name} />
+          {pack.map((cards) => {
+            return <img src={cards.images.small} alt={cards.name} />;
+          })}
         </div>
         <button className="btn btn-primary" onClick={openPack}>
           Open a pack!
@@ -43,7 +49,7 @@ const PackSimulator = () => {
         <div>
           <h1>Pokemon card pack simulator.</h1>
 
-          <button className="btn btn-primary"  disabled onClick={openPack}>
+          <button className="btn btn-primary" disabled onClick={openPack}>
             Open a pack!
           </button>
         </div>
